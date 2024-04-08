c33e215e020b69a2ecc7de4f74a9508915980969
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-08 13:39:52 -0700

    Navbar component init

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between p-4 mx-auto cursor-poiter sm:max-w-4xl md:max-w-6xl'>
+      <h1 className='text-3xl'>
+        Coffee <span className='font-bold'>Shop</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
