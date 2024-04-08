1708f69431c26a799ffc422fd3c421af901644b3
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-08 15:56:05 -0700

    implementing products on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import CarouselTextBanner from "@/components/CarouselTextBanner";
+import FeaturedProducts from "@/components/FeaturedProducts";
 
 export default function Home() {
   return (
     <main>
       <CarouselTextBanner />
+      <FeaturedProducts />
     </main>
   );
 }
