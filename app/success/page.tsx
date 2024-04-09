a9d7ade0711963686e3f4967791325f996ecd588
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-09 15:54:12 -0700

    adding nextjs top loader dependency

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
   return (
     <div className='max-w-5xl p-4 mx-auto sm:py-16 sm:px-24'>
       <div className='flex flex-col-reverse gap-2 sm:flex-row'>
-        <div className='flex justify-center sm:min-w-[400px]'>
+        <div className='flex justify-center md:min-w-[400px]'>
           <Image src='/success.jpg' alt="Success" width={250} height={500} className='rounded-lg' />
         </div>
         <div>
