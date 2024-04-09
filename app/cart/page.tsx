cbd82589ee52a299b4598db985bfeefd37156983
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-09 15:21:13 -0700

    adding stripe logic in frontend

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,12 +6,32 @@ import { useCart } from '@/hooks/useCart'
 import { formatPrice } from '@/lib/formatPrice'
 import React from 'react'
 import CartItem from './components/CartItem'
+import { loadStripe } from '@stripe/stripe-js'
+import { makePaymentRequest } from '@/api/payment'
 
 const Page = () => {
-  const { items, removeAll } = useCart()
+  const { items } = useCart()
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY || '')
 
   const prices = items.map((product => product.attributes.price))
   const totalPrice = prices.reduce((total, price) => total + price, 0)
+
+  const buyStripe = async () => {
+    try {
+      const stripe = await stripePromise
+
+      const response = await makePaymentRequest.post('/api/orders', {
+        products: items
+      })
+
+      await stripe?.redirectToCheckout({
+        sessionId: response.data.stripeSession.id
+      })
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <div className='max-w-6xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
@@ -32,17 +52,17 @@ const Page = () => {
           </ul>
         </div>
         <div className='max-w-xl'>
-            <div className='p-6 rounded-lg bg-slate-100'>
-              <p className='mb-3 text-lg font-semibold dark:text-black'>Order summary</p>
-              <Separator />
-              <div className='flex justify-between gap-5 my-4 dark:text-black'>
-                <p>Order total</p>
-                <p>{formatPrice(totalPrice)}</p>
-              </div>
-              <div className='flex items-center justify-center w-full mt-3'>
-                <Button className='w-full dark:bg-black dark:text-white' onClick={() => console.log("buy")}>Comprar</Button>
-              </div>
+          <div className='p-6 rounded-lg bg-slate-100'>
+            <p className='mb-3 text-lg font-semibold dark:text-black'>Order summary</p>
+            <Separator />
+            <div className='flex justify-between gap-5 my-4 dark:text-black'>
+              <p>Order total</p>
+              <p>{formatPrice(totalPrice)}</p>
             </div>
+            <div className='flex items-center justify-center w-full mt-3'>
+              <Button className='w-full dark:bg-black dark:text-white' onClick={buyStripe}>Comprar</Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
