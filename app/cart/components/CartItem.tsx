fae102323e647c30185d6d4ef69c8e823382b835
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-09 14:58:25 -0700

    abstracting more components

--- app/cart/components/CartItem.tsx
+++ app/cart/components/CartItem.tsx
@@ -1,3 +1,4 @@
+import ProductTasteOrigin from '@/components/shared/ProductTasteOrigin'
 import { useCart } from '@/hooks/useCart'
 import { formatPrice } from '@/lib/formatPrice'
 import { cn } from '@/lib/utils'
@@ -27,14 +28,7 @@ const CartItem = ({ product }: CartItemProps) => {
         <div>
           <h2 className='text-lg font-bold'>{product.attributes.productName}</h2>
           <p>{formatPrice(product.attributes.price)}</p>
-          <div className='flex items-center justify-between gap-3'>
-            <p className='px-2 py-1 text-white bg-black rounded-full dark:bg-white dark:text-black w-fit'>
-              {product.attributes.taste}
-            </p>
-            <p className='px-2 py-1 text-white bg-yellow-900 rounded-full w-fit'>
-              {product.attributes.origin}
-            </p>
-          </div>
+          <ProductTasteOrigin taste={product.attributes.taste} origin={product.attributes.origin} />
         </div>
         <div>
           <button
