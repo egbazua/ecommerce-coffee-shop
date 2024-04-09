94bb12f44593ecb6f08b3fc98bb289de3c3b4c34
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-09 14:52:08 -0700

    abstracting product taste origin component

--- app/loved-products/components/LovedItemProduct.tsx
+++ app/loved-products/components/LovedItemProduct.tsx
@@ -1,3 +1,4 @@
+import ProductTasteOrigin from '@/components/shared/ProductTasteOrigin'
 import { Button } from '@/components/ui/button'
 import { useCart } from '@/hooks/useCart'
 import { useLovedProducts } from '@/hooks/useLovedProducts'
@@ -35,14 +36,7 @@ const LovedItemProduct = ({ product }: LovedItemProductProps) => {
         <div>
           <h2 className='text-lg font-bold'>{product.attributes.productName}</h2>
           <p className='font-bold'>{formatPrice(product.attributes.price)}</p>
-          <div className='flex items-center justify-between gap-3'>
-            <p className='px-2 py-1 text-xs text-white bg-black rounded-full dark:bg-white dark:text-bladck w-fit'>
-              {product.attributes.taste}
-            </p>
-            <p className='px-2 py-1 text-xs text-white bg-yellow-900 rounded-full w-fit'>
-              {product.attributes.origin}
-            </p>
-          </div>
+          <ProductTasteOrigin taste={product.attributes.taste} origin={product.attributes.origin} />
           <Button className='mt-5 rounded-full' onClick={addToCheckout}>Añadir al carrito</Button>
         </div>
         <div>
