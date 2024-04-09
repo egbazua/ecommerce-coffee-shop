7925d2c13f30e7272d5afd8a80f93e94a7d09112
Enrique Bazúa <enriquegbazua@gmail.com>
2024-04-08 17:14:23 -0700

    creating banner product component

---
+++ components/BannerProduct.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+import { buttonVariants } from './ui/button'
+
+const BannerProduct = () => {
+  return (
+    <>
+      <div className='mt-4 text-center'>
+        <p>Sumérgete en una experiencia única</p>
+        <h4 className='mt-2 text-5xl font-extrabold uppercase'>Café Exquisito</h4>
+        <p className='my-2 text-lg'>Despierta tus sentidos con cada sorbo</p>
+        <Link href="#" className={buttonVariants()}>Comprar</Link>
+      </div>
+      <div className="h-[350px] bg-cover lg:h-[600px] bg-[url('/slider-image.jpg')] bg-center mt-5" />
+    </>
+  )
+}
+
+export default BannerProduct
